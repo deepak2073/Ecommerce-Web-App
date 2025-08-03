cc5dbf3984fa6c65947ea868d4d79ae106e307f0
deepak_22scse1012073 <deepakksh2124@gmail.com>
2025-08-04 00:45:49 +0530

    fix cors syntax

--- backend/main.go
+++ backend/main.go
@@ -445,7 +445,7 @@ func setupRoutes() *gin.Engine {
 	r := gin.Default()
 
 	config := cors.Config{
-    	AllowOrigins:     []string{"https://ecommerce-web-app-dowq.vercel.app/",
+    	AllowOrigins:     []string{"https://ecommerce-web-app-dowq.vercel.app/"},
     	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
     	AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
     	AllowCredentials: true,
